c4092f12bd8e11c563fec749505aadcf20fe3774
wangjie11 <1119792237@qq.com>
2026-04-03 11:14:16 +0800

    0403

---
+++ script/glados_checkin.js
@@ -0,0 +1,19 @@
+const cookieVal = 'koa:sess=eyJ1c2VySWQiOjY5ODkyNywiX2V4cGlyZSI6MTgwMTEwMjU4NjE3MCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=p0nJP0PWOZ6cfZuE7Gy1IlMz7hY';
+const header = {
+	Accept: 'application/json, text/plain, */*',
+	'Content-Type': 'application/json;charset=UTF-8',
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
+	Cookie: cookieVal,
+}
+function checkin(){
+	const url = {
+		url: 'https://glados.one/api/user/checkin',
+		headers: {...header},
+	}
+	$httpClient.post(url, (error,response,data)=> {
+		console.log(error,response,data);
+		$notification.post('test','',response)
+		$done()
+	})
+}
+checkin()
