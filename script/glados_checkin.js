cc6c106e8a0e9a03aa88b980a0528b2ca76ce425
wangjie11 <1119792237@qq.com>
2026-04-17 16:50:20 +0800

    0417

--- script/glados_checkin.js
+++ script/glados_checkin.js
@@ -1,4 +1,4 @@
-const cookieVal = 'koa:sess=eyJ1c2VySWQiOjY5ODkyNywiX2V4cGlyZSI6MTgwMTEwMjU4NjE3MCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=p0nJP0PWOZ6cfZuE7Gy1IlMz7hY';
+const cookieVal = '';
 
 const header = {
 	Accept: 'application/json, text/plain, */*',
@@ -6,15 +6,40 @@ const header = {
 	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
 	Cookie: cookieVal,
 }
+function formatAmount(value) {
+	if (value == null) return '未知';
+	const str = String(value);
+
+	return str
+		.replace(/(\.\d*?[1-9])0+$/, '$1')
+		.replace(/\.0+$/, '');
+}
+
 function checkin(){
 	const params = {
 		url: 'https://glados.one/api/user/checkin',
 		headers: {...header},
 		body: JSON.stringify({"token":"glados.one"})
 	};
-	console.log(params);
 	$httpClient.post(params, function(error,response,data){
-		console.log(error,response, data);
+		console.log('end request')
+		if(error){
+			$notification.post('GlaDOS签到错误','',String(error));
+		}else {
+			let json;
+			try{
+				json = JSON.parse(data);
+			}catch(e){
+				$notification.post('GlaDOS返回解析失败', '', String(e));
+				$done();
+				return;
+			}
+			if(json.code === 0){
+				$notification.post('GlaDOS签到结果', '当前积分总计:' + formatAmount(json.list?.[0]?.balance), json.message);
+			}else {
+				$notification.post('GlaDos签到结果', '当前积分总计:' + formatAmount(json.list?.[0]?.balance), json.message, );
+			}
+		}
 		$done();
 	});
 }
